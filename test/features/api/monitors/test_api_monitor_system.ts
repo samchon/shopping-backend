4086a2ddb1cdcbdbbbc8718c31fe2450137df9ba
Jeongho Nam <samchon@wrtn.io>
2023-12-12 03:50:16 +0900

    Fix github actions bug and adjust prettier to test again

--- test/features/api/monitors/test_api_monitor_system.ts
+++ test/features/api/monitors/test_api_monitor_system.ts
@@ -4,10 +4,10 @@ import ShoppingApi from "@samchon/shopping-api";
 import { ISystem } from "@samchon/shopping-api/lib/structures/monitors/ISystem";
 
 export const test_api_monitor_system = async (
-    connection: ShoppingApi.IConnection,
+  connection: ShoppingApi.IConnection,
 ): Promise<void> => {
-    const system: ISystem = await ShoppingApi.functional.monitors.system.get(
-        connection,
-    );
-    typia.assert(system);
+  const system: ISystem = await ShoppingApi.functional.monitors.system.get(
+    connection,
+  );
+  typia.assert(system);
 };
