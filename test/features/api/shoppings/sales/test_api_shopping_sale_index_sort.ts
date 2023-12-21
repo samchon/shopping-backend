ff993a025b8876f928e2cf176883026bd21a4efe
Jeongho Nam <samchon.github@gmail.com>
2023-12-21 14:15:02 +0900

    Fix github actions bug after merging with master

--- test/features/api/shoppings/sales/test_api_shopping_sale_index_sort.ts
+++ test/features/api/shoppings/sales/test_api_shopping_sale_index_sort.ts
@@ -10,8 +10,8 @@ import { IShoppingOrderGood } from "@samchon/shopping-api/lib/structures/shoppin
 import { IShoppingSale } from "@samchon/shopping-api/lib/structures/shoppings/sales/IShoppingSale";
 
 import { ConnectionPool } from "../../../../ConnectionPool";
-import { test_api_shopping_customer_create } from "../actors/test_api_shopping_customer_create";
-import { test_api_shopping_seller_join } from "../actors/test_api_shopping_seller_join";
+import { test_api_shopping_actor_customer_create } from "../actors/test_api_shopping_actor_customer_create";
+import { test_api_shopping_actor_seller_join } from "../actors/test_api_shopping_actor_seller_join";
 import { generate_random_cart_commodity } from "../carts/internal/generate_random_cart_commodity";
 import { generate_random_order } from "../orders/internal/generate_random_order";
 import { generate_random_order_publish } from "../orders/internal/generate_random_order_publish";
@@ -21,10 +21,9 @@ import { generate_random_sale_review } from "./internal/generate_random_sale_rev
 export const test_api_shopping_sale_index_sort = async (
   pool: ConnectionPool,
 ): Promise<void> => {
-  await test_api_shopping_seller_join(pool);
-  const customer: IShoppingCustomer = await test_api_shopping_customer_create(
-    pool,
-  );
+  await test_api_shopping_actor_seller_join(pool);
+  const customer: IShoppingCustomer =
+    await test_api_shopping_actor_customer_create(pool);
   const sale: IShoppingSale = await generate_random_sale(pool);
 
   await ArrayUtil.asyncRepeat(REPEAT)(async () => {
