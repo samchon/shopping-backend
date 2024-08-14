a800c2f332de300ae8d4af6661befc4e8245092e
Jeongho Nam <samchon.github@gmail.com>
2024-08-15 01:20:40 +0900

    Swagger secret value configuration by authentication decorator.

--- src/decorators/ShoppingCustomerAuth.ts
+++ src/decorators/ShoppingCustomerAuth.ts
@@ -2,14 +2,28 @@ import { ExecutionContext, createParamDecorator } from "@nestjs/common";
 import { VariadicSingleton } from "tstl";
 
 import { ShoppingCustomerProvider } from "../providers/shoppings/actors/ShoppingCustomerProvider";
+import { SwaggerCustomizer } from "@nestia/core";
 
-export const ShoppingCustomerAuth = (level?: "guest" | "member" | "citizen") =>
-  singleton.get(level ?? "guest")();
+export const ShoppingCustomerAuth =
+  (level?: "guest" | "member" | "citizen"): ParameterDecorator =>
+  (
+    target: Object,
+    propertyKey: string | symbol | undefined,
+    parameterIndex: number
+  ): void => {
+    SwaggerCustomizer((props) => {
+      props.route.security ??= [];
+      props.route.security.push({
+        bearer: [],
+      });
+    })(target, propertyKey as string, undefined!);
+    singleton.get(level ?? "guest")(target, propertyKey, parameterIndex);
+  };
 
 const singleton = new VariadicSingleton(
   (level: "guest" | "member" | "citizen") =>
     createParamDecorator(async (_0: any, ctx: ExecutionContext) => {
       const request = ctx.switchToHttp().getRequest();
       return ShoppingCustomerProvider.authorize(level)(request);
-    }),
+    })()
 );
--- src/decorators/ShoppingSellerAuth.ts
+++ src/decorators/ShoppingSellerAuth.ts
@@ -2,12 +2,27 @@ import { ExecutionContext, createParamDecorator } from "@nestjs/common";
 import { Singleton } from "tstl";
 
 import { ShoppingSellerProvider } from "../providers/shoppings/actors/ShoppingSellerProvider";
+import { SwaggerCustomizer } from "@nestia/core";
 
-export const ShoppingSellerAuth = () => singleton.get()();
+export const ShoppingSellerAuth =
+  (): ParameterDecorator =>
+  (
+    target: Object,
+    propertyKey: string | symbol | undefined,
+    parameterIndex: number
+  ): void => {
+    SwaggerCustomizer((props) => {
+      props.route.security ??= [];
+      props.route.security.push({
+        bearer: [],
+      });
+    })(target, propertyKey as string, undefined!);
+    singleton.get()(target, propertyKey, parameterIndex);
+  };
 
 const singleton = new Singleton(() =>
   createParamDecorator(async (_0: any, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     return ShoppingSellerProvider.authorize(request);
-  }),
+  })()
 );
