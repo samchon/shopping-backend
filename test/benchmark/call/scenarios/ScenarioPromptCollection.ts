07bd33e9c2989c3bc1ac2492484348fc3b466757
Jeongho Nam <samchon.github@gmail.com>
2025-01-20 16:20:25 +0900

    Change all products to of Apple.
    Requirement by Wrtn, for the 1st demonstration.

--- test/benchmark/call/scenarios/ScenarioPromptCollection.ts
+++ test/benchmark/call/scenarios/ScenarioPromptCollection.ts
@@ -1,7 +1,7 @@
 export namespace ScenarioPromptCollection {
   export const SP9 = [
-    `Would you find "Surface Pro 9" in the market?`,
-    `And take the (i7, 16GB, 512GB) stock to the order.`,
+    `Would you find "iPhone" in the market?`,
+    `And take the "16 Pro Max 1TB" stock to the order.`,
     `I'll pay the order with my cash, and my address is "1234 Main Street, New York, NY, 10001".`,
     `At last, would you check current delivery status of the order?`,
   ];
