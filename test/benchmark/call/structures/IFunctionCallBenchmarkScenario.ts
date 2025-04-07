294bcce1eb4590544a21d3fcfab36fb6c0483b65
Jeongho Nam <samchon.github@gmail.com>
2025-04-07 16:19:11 +0900

    Remove LLM benchmark for a while

--- test/benchmark/call/structures/IFunctionCallBenchmarkScenario.ts
+++
@@ -1,7 +0,0 @@
-import { IFunctionCallBenchmarkExpected } from "./IFunctionCallBenchmarkExpected";
-
-export interface IFunctionCallBenchmarkScenario {
-  title: string;
-  prompt: string;
-  expected: IFunctionCallBenchmarkExpected;
-}
